eb8767301e815b281636ad916de6488af6b985f6
eric610446 <eric610446@gmail.com>
2017-01-10 14:03:53 +0800

    Hello World

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
-<?php
-require "main.php";
-$tpl->assign("title", "測試用的網頁標題");
-$tpl->assign("content", "測試用的網頁內容");
-// 上面兩行也可以用這行代替
-// $tpl->assign(array("title" => "測試用的網頁標題", "content" => "測試用的網頁內容"));
+<?
+// 引用主要的 smarty 資訊
+require "main.php" ;
+
+// 設定變數的內容，這些變數將會在 templates 中對應到 html 檔案裡的變數
+// 建立 Smarty 的變數 title 與 content
+$tpl->assign("title", "First Smarty Program");
+$tpl->assign("content", "Hello World");
+
+//選擇要顯示的框架 
 $tpl->display('index.html');
 
 ?>
--- main.php
+++ main.php
@@ -1,7 +1,15 @@
-<?php
+<?
+// 引用 smarty
 include "library/Smarty/Smarty.class.php";
+
+// 取得 main.php 所在的目錄，並且將 \ 改成 /，定義為 'APP_PATH'
 define('APP_PATH', str_replace('\\', '/', dirname(__FILE__)));
+
+/* 建立 smarty 物件 tpl，定義 tpl 在 smarty 運行後，各個用途的檔案存放的目錄
+	template_dir = 尋找 html 的目錄
+*/
 $tpl = new Smarty();
+
 $tpl->template_dir = APP_PATH . "/templates/";
 $tpl->compile_dir = APP_PATH . "/templates_c/";
 $tpl->config_dir = APP_PATH . "/configs/";
