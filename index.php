7d9cc81f3409143907fb24ac349b8e48ac450117
eric610446 <eric610446@gmail.com>
2017-01-12 15:37:54 +0800

    assign array 測試

--- index.php
+++ index.php
@@ -7,6 +7,12 @@ $tpl -> assign( "title", "Smarty 測試" ) ;
 $tpl -> assign( "content", "Hello World！ 歡迎" ) ;
 $tpl -> assign( "style_dir", "css\default.css" ) ;
 
+$content = array( 
+	0=>"Hello World !",	
+	1=>"It's a Smarty test of assign array.",	
+	2=>"Test success !"	
+	) ;
+$tpl -> assign( "content", $content ) ;
 
 
 $tpl -> display( 'index.html' ) ;
--- templates_c/6462a31e3f3f3704a226c5d01616b968b6917f3e_0.file.index.html.php
+++ templates_c/6462a31e3f3f3704a226c5d01616b968b6917f3e_0.file.index.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-01-12 14:35:54
+/* Smarty version 3.1.30, created on 2017-01-12 15:35:34
   from "C:\AppServ\www\Eric_Test\templates\index.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_587723cabc1d37_30599656',
+  'unifunc' => 'content_587731c66daff0_63537109',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '6462a31e3f3f3704a226c5d01616b968b6917f3e' => 
     array (
       0 => 'C:\\AppServ\\www\\Eric_Test\\templates\\index.html',
-      1 => 1484202952,
+      1 => 1484206252,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_587723cabc1d37_30599656 (Smarty_Internal_Template $_smarty_tpl) {
+function content_587731c66daff0_63537109 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <html>
 
@@ -33,8 +33,12 @@ function content_587723cabc1d37_30599656 (Smarty_Internal_Template $_smarty_tpl)
 
 <body>
 	<article>
-		<?php echo $_smarty_tpl->tpl_vars['content']->value;?>
-
+		<h1><?php echo $_smarty_tpl->tpl_vars['content']->value[0];?>
+</h1>
+		<h3><?php echo $_smarty_tpl->tpl_vars['content']->value[1];?>
+</h3>
+		<p><?php echo $_smarty_tpl->tpl_vars['content']->value[2];?>
+</p>
 	</article>
 </body>
 
