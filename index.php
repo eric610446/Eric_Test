2bd2a03d1537da381e72475517bce9711efdb12f
eric610446 <eric610446@gmail.com>
2017-01-12 15:11:48 +0800

    加個 CSS

--- index.php
+++ index.php
@@ -3,11 +3,12 @@ require "main.php";
 
 
 
-$tpl->assign("title", "Smarty 測試");
-$tpl->assign("content", "Hello World！");
+$tpl -> assign( "title", "Smarty 測試" ) ;
+$tpl -> assign( "content", "Hello World！ 歡迎" ) ;
+$tpl -> assign( "style_dir", "css\default.css" ) ;
 
 
 
-$tpl->display('index.html');
+$tpl -> display( 'index.html' ) ;
 
 ?>
