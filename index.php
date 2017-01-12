92172e8bc73f2e8b80c40fdc514bc4d4985ff3cf
eric610446 <eric610446@gmail.com>
2017-01-12 18:39:51 +0800

    初始化 empty index.php style.css

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-require "main.php";
-$tpl->assign("title", "測試用的網頁標題");
-$tpl->assign("content", "測試用的網頁內容");
-// 上面兩行也可以用這行代替
-// $tpl->assign(array("title" => "測試用的網頁標題", "content" => "測試用的網頁內容"));
-$tpl->display('index.html');
+
+
 
 ?>
